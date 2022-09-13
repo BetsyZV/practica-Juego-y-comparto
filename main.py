d8bd5a9c39bf38288fcc0439c2b4fc28ef5f7056
Betsy Zapata <113310193+BetsyZV@users.noreply.github.com>
2022-09-13 20:22:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Recibir numero e imprimir numeros impares antes del numero
+x= int(input("\nIngresar numero: "))
+for i in range (x,0,)
+
+#Recibir la edad de una persona e imprimir todos los años
+x= int(input("\nIngresar edad: "))
+for i in range(x,0,-1):
+  print (i)
+
+#Solicitar numero de filas e imprimit triangulo
+x= int(input("\nIngresa el número de filas: "))
+for i in range(0,x + 1):
+  for j in range(0,i):
+    print ("+", end =" ")
+  print ("")
+
+#Contador del 10 al 0
+for i in range(10,-1,-1):
+  print (i)
+
+#Repetir hola x veces
+x= int(input("\nIngresa el número de veces que se repetira: "))
+for i in range(0,x):
+  print ("hola")
